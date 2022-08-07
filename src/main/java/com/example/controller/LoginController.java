7de0045ba30c6a10a29865c63a3b678262e712fe
KateySt <kateystetsenko@gmail.com>
2022-08-07 13:53:08 +0300

    add book saller

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.regex.Pattern;
+
 
 @Controller
 public class LoginController {
     @Autowired
     private UserRepository userRepository;
+    private Boolean isPhone = false;
+    private Boolean isEmail = false;
 
     @GetMapping("/login")
     public String getLoginAccount(Model model) {
@@ -27,6 +31,11 @@ public class LoginController {
 
     @PostMapping("/login/creatAccount")
     public String postCreateLogin(@RequestParam String nameUser, @RequestParam String userPhone, @RequestParam String userEmail, @RequestParam String userPassword, Model model) {
+        isPhone = Pattern.matches("^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$", userPhone);
+        isEmail = Pattern.matches("\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*\\.\\w{2,4}", userEmail);
+        if (isPhone == false | isEmail == false) {
+            return "createAccount";
+        }
         User user = new User(true, nameUser, userPhone, userEmail, userPassword);
         user.addRole(userRepository.findOne(2).getRoles().get(0));
         userRepository.save(user);
